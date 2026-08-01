ebb9fd101c4d93aa7378fbd0bc8963526bd9f191
Alma <almamorichetti@gmail.com>
2026-08-01 09:04:29 -0300

    cambio orm y config ts

---
+++ backend/dist/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { MikroORM } from "@mikro-orm/mysql";
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("Backend funcionando");
+});
+async function startServer() {
+    try {
+        const orm = await MikroORM.init();
+        console.log("Conexión a la base de datos exitosa");
+        app.listen(3000, () => {
+            console.log("Servidor levantado en puerto 3000");
+        });
+    }
+    catch (error) {
+        console.error("Error al conectar con la base de datos:", error);
+    }
+}
+startServer();
+//# sourceMappingURL=app.js.map
---
+++ backend/dist/config/mikro-orm.js
@@ -0,0 +1,9 @@
+import { defineConfig } from '@mikro-orm/mysql';
+export default defineConfig({
+    dbName: 'take_away',
+    user: 'root',
+    password: 'alma2005',
+    host: 'localhost',
+    port: 3306,
+});
+//# sourceMappingURL=mikro-orm.js.map
